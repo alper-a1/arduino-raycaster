7469663440ec244380462d34500ae64b00c62142
Squid056 <106012012+Squid056@users.noreply.github.com>
2025-10-05 15:51:04 +1100

    change to 128 value table

--- src/helper/deltadistXtable.py
+++ src/helper/deltadistXtable.py
@@ -1,18 +1,22 @@
 # deltadistXtable.py
+# generate a table of deltadist values for raycasting
+# 128 values precomuted for 1/x from x=1/128 to x=1
+# stored as Q7.8 fixed point (value * 256 rounded to nearest int)
 
 deltadistTable = []
 
-for i in range(1, 128):
-    x = i / 127.0
+for i in range(1, 129):
+    x = i / 128.0
     deltadist = 1.0 / x
     ddfixed8_8 = int(deltadist * 256 + 0.5)
     deltadistTable.append(ddfixed8_8)
 
-# unfortunately, must use int16_t, however works nicely for Q8.8 math later.  
+deltadistTable[0] = 0x7FFF  # make sure we dont overflow int16 into negative
+
+# unfortunately, must use int16_t, however works nicely for Q7.8 math later.  
 c_deltadistXtable = "constexpr int16_t deltaDistTable[] = {"
 c_deltadistXtable += ",".join(str(x) for x in deltadistTable)
 c_deltadistXtable += "};"
 
 # print the generated table cpp copy ready
-# print(c_deltadistXtable)
-print(deltadistTable[63])
+print(c_deltadistXtable)
